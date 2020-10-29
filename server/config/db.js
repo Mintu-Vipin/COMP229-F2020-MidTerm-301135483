9d02e6ab11f1c4cc53f3675937c4f7e3021a2687
Mintu-Vipin <mvipinjo@my.centennialcollege.ca>
2020-10-29 12:45:43 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://mintu:fe5yL1r209DLQCIH@dbserver.lmrrt.mongodb.net/book_store?retryWrites=true&w=majority"
+};
